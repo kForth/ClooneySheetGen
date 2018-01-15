d218ae671e347a1a07166f87363a63236f950d97
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-14 21:27:08 -0500

    remove old ui stuff

--- run.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtWidgets, QtGui
-from ui import MainWindow
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon('img/icon.png'))
-    window = MainWindow()
-    sys.exit(app.exec_())
