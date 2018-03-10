1830c1638f89858f2ea9ee38736823723a88833e
Kestin Goforth <kgoforth1503@gmail.com>
2018-03-10 12:49:55 -0500

    Update sheets

--- generator.py
+++ generator.py
@@ -5,5 +5,5 @@ from sheet import Sheet
 if __name__ == "__main__":
     config = json.load(open("resources/powerup_config.json"))
     gen = Sheet(config)
-    fields = json.load(open("resources/powerup_test_2.json"))
+    fields = json.load(open("resources/powerup_test_3.json"))
     gen.create_from_json(fields)
